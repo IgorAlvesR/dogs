01209920eb704915b94792d355d2ab80ea95ae56
igor.rodrigues <igor@cincatarina.sc.gov.br>
2021-12-21 09:13:35 -0300

    feat:     - cria roteamento     - estiliza header e formulário de login     - cria hook userForm e faz validação     - insere imagens do projeto

---
+++ src/Components/Forms/Input.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Input.module.css";
+
+export const Input = ({
+  label,
+  type,
+  name,
+  onChange,
+  value,
+  error,
+  onBlur,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+      {error && <p className={styles.error}>{error}</p>}
+    </div>
+  );
+};
